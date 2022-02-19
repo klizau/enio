a3f424f3e029c2fd78fd0cd65974aee39db72ced
kuznecsova.Lisa <kuznecsovalisa@gmail.com>
2022-02-19 15:13:36 +0300

    style(portfolio): slider

--- app/js/common.js
+++ app/js/common.js
@@ -3,4 +3,20 @@ $(document).ready(function () {
     arrows: false,
     loop: true,
   });
+
+  $(".slider-portfolio").slick({
+    prevArrow: ".slider-l",
+    nextArrow: ".slider-r",
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+  });
+
+  $(".portfolio-slide").slick({
+    prevArrow: ".slider-left",
+    nextArrow: ".slider-right",
+    infinite: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+  });
 });
